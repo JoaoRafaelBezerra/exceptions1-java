276f14a450ac5bac8bd17fb694f00b7270b4de5b
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-05-23 19:21:53 -0300

    Reading files with BufferedReader and FileReader

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -1,28 +1,42 @@
 package Files;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
-public class Main {// Leitura basica de um arquivo
+public class Main {// Leitura com  FileReader e BufferedReader
 
 	public static void main(String[] args) {
 		
-		File file = new File("C:\\Users\\lnbre\\disgraca.txt");
-		Scanner sc  = null;
+		String path = "C:\\Users\\lnbre\\disgraca.txt";
+		 
+		FileReader fr  = null;
+		BufferedReader br = null;
+		
 		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
+			fr = new FileReader(path);
+		    br = new BufferedReader(fr);
+		    
+		    String line = br.readLine();
+		    
+		    while(line != null) {
+		    	System.out.println(line);
+		    	line = br.readLine();
+		    }
 		}catch(IOException e){
-			System.out.println("Error : " + e.getMessage());
-		}finally {
-			if(sc != null) {
-				sc.close();
-			}
-		}
-
+	    	System.out.println("Error: " + e.getMessage());
+	    }finally {
+	    	try {
+	    		if(br  != null) {
+	    			br.close();
+	    		}
+	    		if(fr != null) {
+	    			fr.close();
+	    		}
+	    	}catch(IOException e) {
+	    		e.printStackTrace();
+	    	}
+	    }
 	}
 
 }
