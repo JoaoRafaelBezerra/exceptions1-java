f8998d659c8fca015fd6388c1c758749d2c3dd28
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-05-29 21:42:13 -0300

    Interfaces part I

---
+++ src/Entities/CarRental.java
@@ -0,0 +1,17 @@
+package Entities;
+
+import java.time.LocalDateTime;
+
+public class CarRental {
+
+	private LocalDateTime start;
+	private LocalDateTime finish;
+	
+	private InVoice inVoice;
+	private Vehicle vehicle;
+	
+	
+	public CarRental() {
+		
+	}
+}
