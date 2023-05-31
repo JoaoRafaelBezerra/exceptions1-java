008507aac02348176dbd6364572c56e01d91cd53
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-05-31 19:32:11 -0300

    Interface problem solution I

--- src/Entities/CarRental.java
+++ src/Entities/CarRental.java
@@ -14,4 +14,43 @@ public class CarRental {
 	public CarRental() {
 		
 	}
+
+	public CarRental(LocalDateTime start, LocalDateTime finish, Vehicle vehicle) {
+		this.start = start;
+		this.finish = finish;
+		this.vehicle = vehicle;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getFinish() {
+		return finish;
+	}
+
+	public void setFinish(LocalDateTime finish) {
+		this.finish = finish;
+	}
+
+	public InVoice getInVoice() {
+		return inVoice;
+	}
+
+	public void setInVoice(InVoice inVoice) {
+		this.inVoice = inVoice;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+	    
 }
--- src/Files/Main.java
+++ src/Files/Main.java
@@ -1,11 +1,28 @@
 package Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.Scanner;
 
-import Entities.InVoice;
+import Entities.CarRental;
+import Entities.Vehicle;
 
 public class Main {//Listing and creating files/folders
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+		System.out.println("Entre com os dados do aluguel:");
+		System.out.print("Modelo do carro: ");
+		String carModel = sc.nextLine();
+		System.out.println("retirada (dd/MM/yyyy HH:mm): ");
+		LocalDateTime start  = LocalDateTime.parse(sc.nextLine(), fmt);
+		System.out.println("retorno (dd/MM/yyyy HH:mm): ");
+		LocalDateTime finish  = LocalDateTime.parse(sc.nextLine(), fmt);
+		
+		CarRental cr = new CarRental(start,finish,new Vehicle(carModel));
 		
 		
 	}
