628a6645e425011ef09dde43f10ef2cf7875b71f
Rafael Bezerra <jbezerra@faeterj-petropolis.edu.br>
2023-06-29 18:49:37 -0300

    Hashcode/Equals

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -2,7 +2,7 @@ package Files;
 
 import java.util.Scanner;
 
-import Services.PrintService;
+import Services.Client;
 
 public class Main {//Listing and creating files/folders
 
@@ -10,18 +10,13 @@ public class Main {//Listing and creating files/folders
 		
 		Scanner sc = new Scanner(System.in);
 		
-		PrintService<String> ps = new PrintService<>();
+		Client client1 = new Client("Maria", "Maria@hamilc.com");
+		Client client2 = new Client("Maria", "Maria@hamilc.com");
 		
-		System.out.println("How many values?");
-		
-		int n = sc.nextInt();
-		
-		for(int i = 0; i < n; i++) {
-			String value = sc.next();
-			ps.addValue(value);
-		}
-		ps.print();
-		System.out.println("First: " + ps.first());
+		System.out.println(client1.hashCode());
+		System.out.println(client2.hashCode());
+		System.out.println(client1.equals(client2));
+		System.out.println(client1 == client2);
 	
 	}
 }
